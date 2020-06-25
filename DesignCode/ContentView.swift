e28ad6ad9c677b8a464b1e0777880eace5209c46
秋 <343836296@qq.com>
2020-06-25 17:02:53 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by 韦秋岑 on 2020/6/25.
+//  Copyright © 2020 weiqiucen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
