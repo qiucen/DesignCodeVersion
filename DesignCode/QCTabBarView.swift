77a154d0cfcec61122700db42f37d2ff531c9b2b
秋 <343836296@qq.com>
2020-06-27 23:52:06 +0800

    完成第一部分

---
+++ DesignCode/QCTabBarView.swift
@@ -0,0 +1,36 @@
+//
+//  QCTabBarView.swift
+//  DesignCode
+//
+//  Created by 韦秋岑 on 2020/6/27.
+//  Copyright © 2020 weiqiucen. All rights reserved.
+//
+
+import SwiftUI
+
+struct QCTabBarView: View {
+    var body: some View {
+        TabView {
+            QCHomeView().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("首页")
+            }
+            QCContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("证书")
+            }
+        }
+        .accentColor(.black) // 设置 Tabbar 的 tintColor
+//        .edgesIgnoringSafeArea(.top) // 忽略顶部安全区域
+    }
+}
+
+// MARK: - 多设备实时预览
+struct QCTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group { // 多设备实时预览
+            QCTabBarView().previewDevice("iPhone 8")
+            QCTabBarView().previewDevice("iPhone 11")
+        }
+    }
+}
