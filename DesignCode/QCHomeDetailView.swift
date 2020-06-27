f8d95773583f89e033fcdf7b988b814a080f5055
秋 <343836296@qq.com>
2020-06-27 17:38:17 +0800

    创建数据模型，动态填充数据

--- DesignCode/QCHomeDetailView.swift
+++ DesignCode/QCHomeDetailView.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-// MARK:  - 提取独立视图
+// MARK:  - 提取的独立视图
 struct QCHomeDetailView: View {
     @Binding var isShowProfile: Bool
     var body: some View {
@@ -25,8 +25,8 @@ struct QCHomeDetailView: View {
             
             ScrollView(.horizontal, showsIndicators: false) { // 创建使用 ScrollView
                 HStack { // 如果要水平滑动，需要将内容放进 HStack 中
-                    ForEach(0 ..< 5) { item in // 循环创建
-                        SectionView()
+                    ForEach(sectionData) { item in // 循环创建
+                        SectionView(section: item) // 填充数据
                     }
                 }
                 .padding(30)
@@ -45,20 +45,22 @@ struct QCHomeDetailView_Previews: PreviewProvider {
     }
 }
 
+// MARK: - 单张课程卡片视图
 struct SectionView: View {
+    var section: SectionModel // 数据模型
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("SwiftUI 中先进的原型设计")
+                Text(section.title)
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(.white)
                 Spacer()
-                Image("Logo")
+                Image(section.logo)
             }
-            Text("18 节课")
+            Text(section.text)
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Image("Illustration1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -66,8 +68,36 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("background9"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("background9").opacity(0.2), radius: 20, x: 0, y: 20)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
+
+
+// MARK: - 基本数据模型
+struct SectionModel: Identifiable {
+    /// `自动生成的唯一标识符`
+    var id = UUID()
+    /// `标题`
+    var title: String
+    /// `文本内容`
+    var text: String
+    /// `文本字符串`
+    var logo: String
+    /// `图像`
+    var image: Image
+    /// `颜色`
+    var color: Color
+}
+
+
+// MARK: - 样本数据
+let sectionData = [
+    SectionModel(title: "SwiftUI 中的原型设计", text: "18 节课", logo: "Logo", image: Image(uiImage: #imageLiteral(resourceName: "Illustration1")), color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))),
+    SectionModel(title: "SwiftUI 创造一个app", text: "20 节课", logo: "Logo", image: Image(uiImage: #imageLiteral(resourceName: "Illustration2")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
+    SectionModel(title: "SwiftUI 的先进之处", text: "20 节课", logo: "Logo", image: Image(uiImage: #imageLiteral(resourceName: "Illustration4")), color: Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))),
+    SectionModel(title: "SwiftUI 可玩性很高", text: "16 节课", logo: "Logo", image: Image(uiImage: #imageLiteral(resourceName: "Illustration3")), color: Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)))
+]
+
+
